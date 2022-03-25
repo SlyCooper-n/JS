9b3ecb095ddd89f538775c0457e10864216620e8
Gabriel <gabrielvitor.frasao@gmail.com>
2022-03-25 19:32:00 -0300

    added script and audio files

---
+++ b7/ex_battery/js/main.js
@@ -0,0 +1,33 @@
+import { Moment } from "moment";
+// JS modules test
+
+const g = (el) => document.querySelector(el);
+const gall = (el) => document.querySelectorAll(el);
+
+const letters = gall(".letters div");
+const input = g("input");
+let inputValue = [];
+
+window.addEventListener("keypress", filterKey);
+input.addEventListener("keypress", filterKey);
+
+function filterKey(event) {
+    let key = event.key;
+    letters.forEach((el) => {
+        if (el.innerText == key.toUpperCase()) {
+            el.style.borderColor = "yellow";
+            setTimeout(() => {
+                el.style.borderColor = "#e5e7eb";
+            }, 500);
+
+            if (event.target == input) {
+                if (!inputValue.includes(`${key.toUpperCase()}`)) {
+                    inputValue.push(key.toUpperCase());
+                }
+            }
+            input.value = "";
+            input.value = inputValue;
+            return;
+        }
+    });
+}
